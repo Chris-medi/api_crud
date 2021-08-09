4e273d234fdeda48ad88043635a0665bee57c7bf
Cristian Medina <ccfranm17@gmail.com>
2021-08-09 09:20:40 -0500

    fix cors in headers

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ const app = express();
 const cors = require('cors');
 app.use(cors())
 
+
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
+
 app.use(express.urlencoded({ extended: false} ));
 app.use(express.json())
 
